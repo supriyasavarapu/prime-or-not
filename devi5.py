fb561da4ef0eeece5b5f621c35f7c5e728de2511
supriyasavarapu <146428159+supriyasavarapu@users.noreply.github.com>
2025-07-08 20:54:06 +0700

    Add files via upload

---
+++ devi5.py
@@ -0,0 +1,13 @@
+#############prime factor or not ##############
+
+
+# n=int(input())
+# count=0
+# for i in range(2,n-1):
+#     if (n%i==0):
+#         count +=1
+# if (count>=1):
+#     print("not prime")
+# else:                                                   # o/p=6
+#     print("prime")                                       #    not prime
+    
